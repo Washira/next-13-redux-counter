cd6ae95a95fc9f741c5379e526a58fe15c56dce3
Washira <us_pou@hotmail.com>
2023-07-18 08:46:59 +0700

    Prove to use state management to any components

--- src/pages/stock/index.jsx
+++ src/pages/stock/index.jsx
@@ -1,20 +1,23 @@
+import React from 'react'
+import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { add, remove } from '@/store/slices/appSlice'
-import Link from 'next/link';
-import styles from './index.module.css'
+import styles from './stock.module.css'
 
-export default function Home() {
-  // https://redux-toolkit.js.org/tutorials/quick-start
+const Stock = () => {
   const appReducer = useSelector((state) => state.appReducer)
   const dispath = useDispatch()
   return (
     <div>
       <nav>
-        <Link href="/stock" className={styles.btn}>Stock</Link>
+        <Link href="/" className={styles.btn}>Root</Link>
       </nav>
+      <hr />
       <button onClick={()=>dispath(remove())}>-</button>
       <div>Counter: {appReducer.count1}</div>
       <button onClick={()=>dispath(add())}>+</button>
     </div>
   )
 }
+
+export default Stock
